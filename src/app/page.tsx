ce94e184c95cdc6b36af9a0603d57f13c860360d
meahmadhassan <meahmadhassan@users.noreply.github.com>
2024-09-10 21:43:16 +0800

    init app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
+
+export default function Home() {
+  return (
+    // <div className="text-red-500 font-bold text-2xl">
+    //   Home page
+    // </div>
+    // <Button variant="slack" > Click me </Button>
+    <AuthScreen />
+  );
+}
